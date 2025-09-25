f5f12963ab39485a5fc1694f3b0616548a807072
patrick26433 <mwihu.26433@students.kyu.ac.ke>
2025-09-25 22:37:32 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,15 @@
+/*
+Name:Patrick kanyora
+Reg No:CT101/G/26433/25
+Description:prompt user to enter bank balance
+*/
+
+#include<stdio.h>
+
+int main(){
+    int bankbalance;
+printf("enter your bankbalance is:");
+scanf("%d",&bankbalance);
+printf("your bankbalance is%d",bankbalance);
+return 0;
+}
