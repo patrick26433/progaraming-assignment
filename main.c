6bf513b3efd0d48ddb94aca909f8f7504b2b33af
patrick26433 <mwihu.26433@students.kyu.ac.ke>
2025-10-02 23:56:16 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,1 +1,42 @@
+/*
+Name:Patrick kanyora
+Reg No:CT101/G/26433/25
+Date:2 oct 2025
+Description:mobile data bundle purchase
+*/
 
+#include<stdio.h>
+
+int main(){
+    int choice;
+
+    //display the menu
+    printf("Select data bundle: \n ");
+    printf("1. 100MB @50KES  \n ");
+    printf("2. 500MB @200KES \n");
+    printf("3. 1GB   @350KES \n");
+    printf("4. 2GB   @600KES \n");
+
+    //prompt user to enter choice
+    printf("Enter your choice(1-4):");
+    scanf("%d",&choice);
+
+    //use switch-case
+    switch(choice){
+    case 1:
+        printf("You selected 100MB. cost=50KES \n");
+        break;
+    case 2:
+        printf("You selected 500MB. cost=200KES \n");
+        break;
+    case 3:
+        printf("You selected 1 GB. cost=350KES \n");
+        break;
+    case 4:
+        printf("You selected 2 GB. cost=600KES \n");
+        break;
+    default:
+        printf("Invalid choice \n");
+    }
+    return 0;
+}
