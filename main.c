a8ad9ee0e224888c6eb0e99c78486e763113d6fb
patrick26433 <mwihu.26433@students.kyu.ac.ke>
2025-10-09 23:26:09 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,17 @@
+/*
+Name:Patrick kanyora
+Reg No:CT101/G/26433/25
+Descrition:Bank atm withdrawal
+*/
+
+#include<stdio.h>
+
+int main(){
+    int i;
+
+    //print numbers 100 to 50
+    for(i=100;i>=50;i--){
+        printf("%d \n",i);
+    }
+    return 0;
+}
