d8a27f86a4f0f1aa827e312d32c60dd5c3001422
patrick26433 <mwihu.26433@students.kyu.ac.ke>
2025-10-23 19:21:32 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,25 @@
+/*
+Name:Patrick kanyora
+Reg No;CT101/G/26433/25
+Description:converttocelsius
+*/
+
+#include<stdio.h>
+int converttocelsius(float fahrenheit);
+void main(){
+   float fahrenheit,celsius;
+   //prompt user to enter fahrenheit
+   printf("Enter fahrenheit:");
+   scanf("%f",&fahrenheit);
+
+   //function calling
+   celsius=converttocelsius(fahrenheit);
+
+   //print the celsius
+   printf("The temperature in celsius is: %.2f\n",celsius);
+}
+int converttocelsius(float fahrenheit){
+    float celsius;
+    celsius=(fahrenheit-32)*5/9;
+    return celsius;
+}
